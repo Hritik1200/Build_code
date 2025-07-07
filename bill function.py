a05e88a4edb65b277dda3a06c360f6ea7abcf302
Hritik lad <ladhritik863@gmail.com>
2025-07-07 20:46:31 +0530

    Update bill function.py

--- bill function.py
+++ bill function.py
@@ -10,4 +10,17 @@ def bill (price,tax,quantity):
     print(f"GST Amount = {gst_amount}")
     print(f"Total Amount + GST = {total_amount}")
 
-bill(3000,18,20)
+bill(3000,18,20)   # change values according to you
+
+
+#  TO print multiple bills 
+
+price = [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]  # change values according to you
+tax = [2,4,6,8,12,18,16,12,10,8]   ## change values according to you
+quantity = [10,20,30,40,50,60,70,80,90,100]  # change values according to you
+
+size = len(price)
+
+for i in range(size):
+    bill(price[i],tax[i],quantity[i])
+
