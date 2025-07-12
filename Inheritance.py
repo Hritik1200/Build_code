ab0dd9596d7533566467e30addee11123e8d2ace
Hritik lad <ladhritik863@gmail.com>
2025-07-12 11:38:14 +0530

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -1,3 +1,75 @@
+# this is updated Credit Card Transaction System using Python Inheritance
+# in this we can can call same function card1 multiple times to print Amount which will
+# be added in total limit
+
+class customer:
+    def info(id,n):
+        print("Customer ID :",id,"Customer Name: ",n)
+
+class Bank(customer):
+    def bank_AC(card_type,card_number):
+        print("Card Type:",card_type,"Card Number:",card_number)
+        
+Limit1 = 0
+class Creditc1(Bank):
+    def card1(tran1,limit1):
+        global Limit1
+        Limit1 += limit1
+        print("Transaction ID:",tran1," Amount:",limit1)
+
+class Bank1(Creditc1):
+    def bank_AC1(card_type1,card_number1):
+        print("Card Type:",card_type1,"Card Number:",card_number1)
+
+class Credit_limit(Bank1):
+    def All_info():
+        print("Total Transaction Amount:",Limit1)
+
+# this method is to take user input
+a = Credit_limit
+a.info(input("Customer id"),input("Enter name"))
+a.bank_AC(input("Enter card type"),input("Enter card No"))
+a.card1(input("Enter Transaction ID1:"),int(input("enter limit1")))
+a.bank_AC1(input("Enter card type"),input("Enter card No"))
+a.All_info()
+
+# we have to run this in different cell or every time we have to give all input
+a.card1(input("Enter Transaction ID1:"),int(input("enter limit1")))
+a.All_info()
+
+
+# if we dont want to change value every time when we run code
+
+a = Credit_limit
+a.info(123,"Hritik")
+a.bank_AC("Visa",880567840)
+a.card1("T001",6000)
+a.bank_AC1("Amex",7890456738)
+a.All_info()
+
+# this is how we can call card1 function multiple times
+
+a.card1("T002",6000)
+a.All_info()
+a.card1("T003",68900)
+a.All_info()
+
+# Output
+
+Customer ID : 123 Customer Name:  Hritik
+Card Type: Visa Card Number: 880567840
+Transaction ID: T001  Amount: 6000
+Card Type: Amex Card Number: 7890456738
+Total Transaction Amount: 6000
+Transaction ID: T002  Amount: 6000
+Total Transaction Amount: 12000
+Transaction ID: T003  Amount: 68900
+Total Transaction Amount: 80900
+
+
+
+# Credit Card Transaction System using Python Inheritance
+
 class customer:
     def info():
         print("Customer Name : Hritik")
@@ -30,43 +102,7 @@ a.card2(4000)
 a.All_info()
 
 
-# using input method
-class customer:
-    def info(id,n):
-        print("Customer ID :",id,"Customer Name: ",n)
 
-class Bank(customer):
-    def bank_AC(card_type,card_number):
-        print("Card Type:",card_type,"Card Number:",card_number)
 
-class Creditc1(Bank):
-    def card1(tran1,limit1):
-        global Limit1
-        Limit1 = limit1
-        print("Transaction ID:",tran1," Amount:",limit1)
-    def card2(tran2,limit2):
-        global Limit2
-        Limit2 = limit2
-        print("Transaction ID:",tran2,"Amount:",limit2)
 
-class Bank1(Creditc1):
-    def bank_AC1(card_type1,card_number1):
-        print("Card Type:",card_type1,"Card Number:",card_number1)
-    def card3(tran3,limit3):
-        global Limit3
-        Limit3 = limit3
-        print("Transaction ID:",tran3,"Amount:",limit3)
-
-class Credit_limit(Bank1):
-    def All_info():
-        print("Total Transaction Amount:",Limit1+Limit2+Limit3)
-
-a = Credit_limit
-a.info(input("Customer id"),input("Enter name"))
-a.bank_AC(input("Enter card type"),input("Enter card No"))
-a.card1(input("Enter Transaction ID1:"),int(input("enter limit1")))
-a.card2(input("Enter Transaction ID2:"),int(input("enter limit2")))
-a.bank_AC1(input("Enter card type"),input("Enter card No"))
-a.card3(input("Enter Transaction ID3:"),int(input("enter limit3")))
-a.All_info()
 
